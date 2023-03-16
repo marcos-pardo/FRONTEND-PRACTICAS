65d48676881259e2bc71f836e8b17dceab489444
marcos-pardo <101109006+marcos-pardo@users.noreply.github.com>
2023-03-16 18:33:03 +0100

    Add files via upload

---
+++ practica3/practica_3/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import HouseComponent from '@/componentes/casa-componentes';
+import MagoComponent from '@/componentes/mago-componentes';
+
+export default function Home() {
+  return (
+<>
+<HouseComponent/>
+</>
+  );
+}
---
+++ practica3/practica_3/src/pages/wizard.tsx
@@ -0,0 +1,9 @@
+import MagoComponent from '@/componentes/mago-componentes';
+
+export default function Wizard() {
+    return (
+        <>
+            <MagoComponent />
+        </>
+    );
+}
